f02638bf15c003f8d06d499f730e769f2c1ac880
mdqueiroz1 <mdqueiroz12@gmail.com>
2021-11-21 11:34:30 -0300

    alt finas no header e no component login

--- LojaCast/src/app/angular-material/angular-material.module.ts
+++ LojaCast/src/app/angular-material/angular-material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
@@ -10,7 +12,9 @@ import {MatSidenavModule} from '@angular/material/sidenav';
   ],
   exports:[
     MatSnackBarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 
