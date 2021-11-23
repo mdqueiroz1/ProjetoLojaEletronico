d9da5aefe7829029eab52582e27ddff869f919e2
mdqueiroz1 <mdqueiroz12@gmail.com>
2021-11-23 19:14:47 -0300

    atualização de imports

--- LojaCast/src/app/angular-material/angular-material.module.ts
+++ LojaCast/src/app/angular-material/angular-material.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSnackBarModule} from '@angular/material/snack-bar';
-import { MatSidenavModule} from '@angular/material/sidenav';
-import { MatMenuModule} from '@angular/material/menu';
-import { MatIconModule} from '@angular/material/icon';
-import { MatInputModule} from '@angular/material/input';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import { MatStepperModule} from '@angular/material/stepper';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatOptionModule } from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
 
 
 
--- LojaCast/src/app/app.module.ts
+++ LojaCast/src/app/app.module.ts
@@ -1,33 +1,19 @@
-import { NgModule } from '@angular/core';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireModule } from '@angular/fire';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { Page404Component } from './page404/page404.component';
-import { CadastroComponent } from './cadastro/cadastro.component';
-
 import { AuthService } from './auth.service';
-import { PagamentoComponent } from './pagamento/pagamento.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { QuemSomosComponent } from './quem-somos/quem-somos.component';
-import { EntregaEfreteComponent } from './entrega-efrete/entrega-efrete.component';
-import { PoliticaEtrocaComponent } from './politica-etroca/politica-etroca.component';
-import { AngularMaterialModule } from './angular-material/angular-material.module';
-import { GridProdutosComponent } from './grid-produtos/grid-produtos.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { DescricaoProdutoComponent } from './descricao-produto/descricao-produto.component';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { DescricaoProduto2Component } from './descricao-produto2/descricao-produto2.component';
 import { DescricaoProduto3Component } from './descricao-produto3/descricao-produto3.component';
 import { DescricaoProduto4Component } from './descricao-produto4/descricao-produto4.component';
@@ -36,6 +22,16 @@ import { DescricaoProduto6Component } from './descricao-produto6/descricao-produ
 import { DescricaoProduto7Component } from './descricao-produto7/descricao-produto7.component';
 import { DescricaoProduto8Component } from './descricao-produto8/descricao-produto8.component';
 import { DescricaoProduto9Component } from './descricao-produto9/descricao-produto9.component';
+import { EntregaEfreteComponent } from './entrega-efrete/entrega-efrete.component';
+import { FooterComponent } from './footer/footer.component';
+import { GridProdutosComponent } from './grid-produtos/grid-produtos.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PagamentoComponent } from './pagamento/pagamento.component';
+import { Page404Component } from './page404/page404.component';
+import { PoliticaEtrocaComponent } from './politica-etroca/politica-etroca.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 const configFirebase = {
   apiKey: "AIzaSyBvWbroFKEazUNgLekqjR29qAl_38P0OAE",
@@ -70,7 +66,7 @@ const configFirebase = {
     DescricaoProduto7Component,
     DescricaoProduto8Component,
     DescricaoProduto9Component,
-    
+
   ],
   imports: [
     CommonModule,
